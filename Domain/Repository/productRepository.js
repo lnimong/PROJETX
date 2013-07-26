8703a402ae7d9d17cbedea11c3096d6143c4def7
Rad <Rad_hass@hotmail.com>
2013-07-26 18:39:31 +0200

    Modification du fake (suppression de Product)

--- Domain/Repository/productRepository.js
+++ Domain/Repository/productRepository.js
@@ -6,10 +6,10 @@ var db = new neo4j.GraphDatabase('http://localhost:7474');
 	};
 
 	exports.getProduct = function (id) {
-		return {'product':{ 'name':'t-shirt bleu', 'imageUrl':'http://ecx.images-amazon.com/images/I/41o8ASd%2B%2B%2BL._SX342_.jpg', 'price':50, 'id':1, 'sizes':['M','L', 'XL']}};
+		return { 'name':'t-shirt bleu', 'imageUrl':'http://ecx.images-amazon.com/images/I/41o8ASd%2B%2B%2BL._SX342_.jpg', 'price':50, 'id':1, 'sizes':['M','L', 'XL']};
 	};
 
-	exports.getSuggestions = function () {
+	exports.getSuggestions = function () { 
 		return [ {'product':{  'name':'t-shirt bleu', 'description': '', 'imageUrl':'http://ecx.images-amazon.com/images/I/41o8ASd%2B%2B%2BL._SX342_.jpg', 'price':50, 'id':1} }, 
     	{'product':{ 'name':'pantalon', 'imageUrl':'http://ecx.images-amazon.com/images/I/41o8ASd%2B%2B%2BL._SX342_.jpg', 'price':50, 'id':2}}, 
     	{'product':{ 'name':'chaussure marron', 'imageUrl':'http://ecx.images-amazon.com/images/I/41o8ASd%2B%2B%2BL._SX342_.jpg', 'price':33.5, 'id':3}}];
