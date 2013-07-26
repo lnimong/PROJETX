e4c1db27e39a39f124f2afc8d64913a7f751d204
Rad <Rad_hass@hotmail.com>
2013-07-26 15:44:37 +0200

    changement imageUrl en place de urlImage (laurent arrête la fumette)

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,11 +1,11 @@
 
 exports.all = function(req, res){
     res.send(
-    	[ {'product':{  'name':'t-shirt bleu', 'urlImage':'http://g-ecx.images-amazon.com/images/G/08/apparel/rcxgs/tile._V211468500_.gif', 'price':50, 'id':1} }, 
-    	{'product':{ 'name':'pantalon', 'urlImage':'http://g-ecx.images-amazon.com/images/G/08/apparel/rcxgs/tile._V211468500_.gif', 'price':50, 'id':2}}, 
-    	{'product':{ 'name':'chaussure marron', 'urlImage':'http://g-ecx.images-amazon.com/images/G/08/apparel/rcxgs/tile._V211468500_.gif', 'price':33.5, 'id':3}}]);
+    	[ {'product':{  'name':'t-shirt bleu', 'imageUrl':'http://g-ecx.images-amazon.com/images/G/08/apparel/rcxgs/tile._V211468500_.gif', 'price':50, 'id':1} }, 
+    	{'product':{ 'name':'pantalon', 'imageUrl':'http://g-ecx.images-amazon.com/images/G/08/apparel/rcxgs/tile._V211468500_.gif', 'price':50, 'id':2}}, 
+    	{'product':{ 'name':'chaussure marron', 'imageUrl':'http://g-ecx.images-amazon.com/images/G/08/apparel/rcxgs/tile._V211468500_.gif', 'price':33.5, 'id':3}}]);
 };
 
 exports.product = function(req, res){
-    res.send({'product':{ 'name':'t-shirt bleu', 'urlImage':'http://g-ecx.images-amazon.com/images/G/08/apparel/rcxgs/tile._V211468500_.gif', 'price':50, 'id':1, 'sizes':['M','L', 'XL']}});
+    res.send({'product':{ 'name':'t-shirt bleu', 'imageUrl':'http://g-ecx.images-amazon.com/images/G/08/apparel/rcxgs/tile._V211468500_.gif', 'price':50, 'id':1, 'sizes':['M','L', 'XL']}});
 };
