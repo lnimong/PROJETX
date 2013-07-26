c2aa0aadc96efebdce6ecbdf73d51c6655629007
Rad <Rad_hass@hotmail.com>
2013-07-26 15:10:24 +0200

    Ajout API avec implémentation fake

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 var home = require('./home');
+var cart = require('./api/cart');
+var products = require('./api/products');
 
 exports.register = function (app) {
   /* views */
   app.get('/', home.index);
+
+  /* api */
+  app.get('/api/products/', products.all);
+  app.get('/api/products/:id', products.product);
+
+  app.post('api/cart/', cart.addToCart);
 };
