19962673cb12b021b2de07f06a467fded8b9ea40
lnimong <lnimong@gmail.com>
2013-07-26 17:30:02 +0200

    nouvelle page catalog version 0

--- public/js/controllers/catalog-ctrl.js
+++ public/js/controllers/catalog-ctrl.js
@@ -1,11 +1,13 @@
 define(function () {
   'use strict';
 
-  function CatalogCtrl ($scope) {
-
+  function CatalogCtrl ($scope, productSrv) {
+  		productSrv.getProducts(function(products) {
+  			$scope.products = products;
+  		})		
   }
 
-  CatalogCtrl.$inject = [ '$scope' ];
+  CatalogCtrl.$inject = [ '$scope', 'productSrv' ];
 
   return CatalogCtrl;
 });
--- public/js/services/product-srv.js
+++ public/js/services/product-srv.js
@@ -1,11 +1,23 @@
 define(function () {
   'use strict';
 
-  function ProductSrv ($http) {
+  	function ProductSrv ($http) {
 
-  }
+	  	this.getProducts = function (callback) {
+		    $http({method: 'GET', url: '/api/products/'}).
+		    success(function(data, status, headers, config) {
+		    	callback(data);
+		    }).
+		    error(function(data, status, headers, config) {
+
+		    });
+	  	}
+
+	}
 
   ProductSrv.$inject = [ '$http' ];
+
+
 
   return ProductSrv;
 });
