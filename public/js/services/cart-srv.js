4a2cab1274436b394596b9d5b07df425cae45b2f
Loïc Fontaine <ljph.fontaine@gmail.com>
2013-08-02 16:45:06 +0200

    Added the colors on the product page. The stock is refreshed on size and/or color change.

--- public/js/services/cart-srv.js
+++ public/js/services/cart-srv.js
@@ -1,17 +1,18 @@
 define(function () {
   'use strict';
 
-  function CartSrv ($http) {
-    this.addToCart = function (product, size) {
+  function CartSrv ($http, $window) {
+
+    this.addToCart = function (product) {
       return $http
-        .post('/api/cart', { modelId: product.modelId, size: size })
-        .error(function (data, status) {
-          deferred.reject(status);
+        .post('/api/cart', { modelId: product.modelId })
+        .error(function (data, status, headers, config) {
+          $window.alert('Oups: ' + status);
         });
     };
   }
 
-  CartSrv.$inject = [ '$http' ];
+  CartSrv.$inject = [ '$http', '$window' ];
 
   return CartSrv;
 });
