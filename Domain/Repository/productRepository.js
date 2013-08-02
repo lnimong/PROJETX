b015b160808b13c942933bed06055f97056338bd
Lepinay Laurent <lepinay@gmail.com>
2013-08-02 15:47:47 +0200

    config neo4j

--- Domain/Repository/productRepository.js
+++ Domain/Repository/productRepository.js
@@ -5,6 +5,7 @@ var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 var getAllProductsQuery = fs.readFileSync(__dirname +"/CypherQueries/getAllProductsQuery.cql", "UTF8");
 var getProduct = fs.readFileSync(__dirname +"/CypherQueries/getProduct.cql", "UTF8");
+var getSuggestions = fs.readFileSync(__dirname +"/CypherQueries/getSuggestions.cql", "UTF8");
 
 exports.getAllProducts = function (onResults) {
             db.query(getAllProductsQuery, {}, onResults);
@@ -19,15 +20,5 @@ exports.getProductStock = function (id, color, size, onResults) {
 };
 
 exports.getSuggestions = function (modelId, onResults) { 
-        db.query("START model=node:node_auto_index(mid={id}) "+ 
-                  "MATCH (genre)-[:CONVIENT_A]-(model)<-[:DE_MODELE]-(produit) " +
-                                             "-[:EST_UN]->(template) " + 
-                                             "-[:S_ASSOCIE_AVEC]-(complement) " +
-                                             "<-[:EST_UN]-(comp_produit) " +
-                                             "-[:DE_MODELE]->(comp_model) " +
-                                             "-[:CONVIENT_A]->(produit_sex) " +
-                  "WHERE produit_sex.sex = genre.sex " +
-                  "RETURN DISTINCT comp_produit.name as name, comp_produit.description as description, " +
-                                   "comp_produit.imageUrl as imageUrl, comp_produit.price as price, " +
-                                   "comp_produit.id as id;", {id:modelId}, onResults);
+        db.query(getSuggestions, {mid:modelId}, onResults);
 };
